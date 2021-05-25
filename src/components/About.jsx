ad495c92e69ba86a19a6a2b2704f0bbd8bce4985
pphos <coc4h8conhc6h12nh@gmail.com>
2021-05-25 20:38:59 +0900

    fix About style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 
+import Title from '../components/shared/Title';
+
 const About = () => {
   return (
-    <section className="about section" id="about">
-      <div className="inner">
-        <h2 className="title">ABOUT</h2>
-        <div className="profile">
-          <p className="profile-img">
-            <img src="/images/profile.jpg" alt="" />
+    <section className="py-20" id="about">
+      <div className="max-w-7xl my-0 mx-auto px-2">
+        <Title>ABOUT</Title>
+        <div className="flex flex-wrap max-w-4xl my-0 mx-auto">
+          <p className="w-1/5 mr-9">
+            <img className="rounded-full" src="/images/profile.jpg" alt="" />
           </p>
-          <div className="profile-body">
+          <div className="flex-1">
             <p>
               テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります。
-            </p>
-            <p>
-              テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります。
             </p>
           </div>
         </div>
