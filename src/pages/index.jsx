06be01629b6447b5decc1593f03be1bb501aeb41
pphos <coc4h8conhc6h12nh@gmail.com>
2021-05-28 20:56:57 +0900

    initialize gatsby project

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import Works from '../components/Works';
-import Skill from '../components/Skill';
-import About from '../components/About';
-import Footer from '../components/Footer';
 
 const Home = () => {
   return (
-    <div className="pt-10">
-      <Header/>
-      <main className="content">
-        <Hero />
-        <Works />
-        <Skill />
-        <About />
-      </main>
-      <Footer/>
-    </div>
+    <h1>Hello</h1>
   );
 };
 
