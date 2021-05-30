b9bbbd66928eb87fb8dacaed3639d064362c6c58
pphos <coc4h8conhc6h12nh@gmail.com>
2021-05-30 15:25:26 +0900

    add Hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Flex, Heading, VStack, Text } from '@chakra-ui/react';
+
+const Hero: React.VFC = () => {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      height="85vh"
+      bgImage="linear-gradient(to left, rgba(54, 209, 220, 0.7) , rgba(91, 134, 229, 0.8)), url('/images/hero_image.jpg')"
+    >
+      <VStack spacing="1">
+        <Heading size="2xl" letterSpacing="0.1em" color="white">
+          PPHOS
+        </Heading>
+        <Text
+          fontSize={{ base: 'lg', md: 'xl' }}
+          fontWeight="bold"
+          letterSpacing="0.1em"
+          color="white"
+        >
+          PORTFOLIO
+        </Text>
+        <Text fontSize={{ base: 'md', md: 'lg' }} color="white" pt="3">
+          私のポートフォリオサイトです。
+        </Text>
+      </VStack>
+    </Flex>
+  );
+};
+
+export default Hero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Home: React.VFC = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
